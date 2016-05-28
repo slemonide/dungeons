c04edca5bad67028c11483d34d38453931788075
Lemon Slemon <slemonide@gmail.com>
2016-05-28 08:41:33 -0700

    Make nicer walls

--- init.lua
+++ init.lua
@@ -2,6 +2,18 @@ local SEED = minetest.get_mapgen_params().seed
 local MAX = 0.2
 local AREA = 50 -- Controls the minumum sizes of the walls
 
+-- Nodes that can make the walls
+local NODES = {
+	"default:stone",
+	"default:cobble",
+	"default:mossycobble",
+	"default:stonebrick",
+	"default:sandstonebrick",
+	"default:desert_stonebrick",
+	"default:obsidianbrick",
+	"default:brick",
+}
+
 -- Returns a boolean for a seed. Use MAX to control the probability.
 local cache = {}
 local function get_randomseed_boolean(seed)
@@ -11,10 +23,10 @@ local function get_randomseed_boolean(seed)
 		math.randomseed(seed)
 		if math.random() <= MAX then
 			cache[seed] = true
-			return true
+			get_randomseed_boolean(seed)
 		else
 			cache[seed] = false
-			return false
+			get_randomseed_boolean(seed)
 		end
 	end
 end
@@ -35,11 +47,12 @@ minetest.register_on_generated(function(minp, maxp, seed)
 				if x < 100 then -- for debug
 
 					local p_pos = area:index(x, y, z)
-					if get_randomseed_boolean(SEED + x + math.floor(math.sin(2*math.pi*(y + z) / AREA)))
-					or get_randomseed_boolean(SEED + y + math.floor(math.sin(2*math.pi*(x + y) / AREA)))
-					or get_randomseed_boolean(SEED + z + math.floor(math.sin(2*math.pi*(x + y) / AREA))) then
+					if get_randomseed_boolean(SEED + x + math.floor((y + z) / AREA))
+					or get_randomseed_boolean(SEED + y + math.floor((x + z) / AREA))
+					or get_randomseed_boolean(SEED + z + math.floor((x + y) / AREA)) then
 
-						data[p_pos] = minetest.get_content_id("default:stone")
+						local node = NODES[math.random(#NODES)]
+						data[p_pos] = minetest.get_content_id(node)
 					end
 				end
 			end
@@ -61,7 +74,22 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	minetest.chat_send_all(geninfo)
 end)
 
+-- Minetest lightning system sucks
+minetest.register_node("dungeons:light", {
+    description = "Light",
+    drawtype = "airlike",
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    climbable = false,
+    paramtype = "light",
+    light_source = 12,
+    sunlight_propagates = true,
+    groups = {not_in_creative_inventory=1},
+})
 
+minetest.register_alias("mapgen_singlenode", "dungeons:light")
 minetest.register_on_mapgen_init(function(params) -- Automatically turn on singlenode generator
 	minetest.set_mapgen_params({
 		mgname = "singlenode"
